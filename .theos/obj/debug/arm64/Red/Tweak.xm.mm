e3e783b33ac118a9318cb1a5b0663857380794cd
1215clf <1210680838@qq.xom>
2019-09-26 14:58:33 +0800

    增加过滤

--- .theos/obj/debug/arm64/Red/Tweak.xm.mm
+++ .theos/obj/debug/arm64/Red/Tweak.xm.mm
@@ -26,9 +26,9 @@
 #define _LOGOS_RETURN_RETAINED
 #endif
 
-@class WCRedEnvelopesLogicMgr; @class WCTableViewSectionManager; @class MMServiceCenter; @class CContactMgr; @class WCTableViewCellManager; @class CMessageMgr; @class WCBizUtil; @class NewSettingViewController; 
+@class CContactMgr; @class WCTableViewCellManager; @class WCBizUtil; @class NewSettingViewController; @class WCTableViewSectionManager; @class CMessageMgr; @class WCRedEnvelopesLogicMgr; @class MMServiceCenter; 
 static void (*_logos_orig$_ungrouped$NewSettingViewController$reloadTableData)(_LOGOS_SELF_TYPE_NORMAL NewSettingViewController* _LOGOS_SELF_CONST, SEL); static void _logos_method$_ungrouped$NewSettingViewController$reloadTableData(_LOGOS_SELF_TYPE_NORMAL NewSettingViewController* _LOGOS_SELF_CONST, SEL); static void (*_logos_orig$_ungrouped$WCRedEnvelopesLogicMgr$OnWCToHongbaoCommonResponse$Request$)(_LOGOS_SELF_TYPE_NORMAL WCRedEnvelopesLogicMgr* _LOGOS_SELF_CONST, SEL, HongBaoRes *, HongBaoReq *); static void _logos_method$_ungrouped$WCRedEnvelopesLogicMgr$OnWCToHongbaoCommonResponse$Request$(_LOGOS_SELF_TYPE_NORMAL WCRedEnvelopesLogicMgr* _LOGOS_SELF_CONST, SEL, HongBaoRes *, HongBaoReq *); static void (*_logos_orig$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$)(_LOGOS_SELF_TYPE_NORMAL CMessageMgr* _LOGOS_SELF_CONST, SEL, NSString *, CMessageWrap *); static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_SELF_TYPE_NORMAL CMessageMgr* _LOGOS_SELF_CONST, SEL, NSString *, CMessageWrap *); 
-static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$MMServiceCenter(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("MMServiceCenter"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCRedEnvelopesLogicMgr(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCRedEnvelopesLogicMgr"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCTableViewSectionManager(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCTableViewSectionManager"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$CContactMgr(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("CContactMgr"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCTableViewCellManager(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCTableViewCellManager"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCBizUtil(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCBizUtil"); } return _klass; }
+static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$MMServiceCenter(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("MMServiceCenter"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCTableViewCellManager(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCTableViewCellManager"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCBizUtil(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCBizUtil"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCRedEnvelopesLogicMgr(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCRedEnvelopesLogicMgr"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$WCTableViewSectionManager(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("WCTableViewSectionManager"); } return _klass; }static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$CContactMgr(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("CContactMgr"); } return _klass; }
 #line 7 "Red/Tweak.xm"
 
 static void _logos_method$_ungrouped$NewSettingViewController$reloadTableData(_LOGOS_SELF_TYPE_NORMAL NewSettingViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd){
@@ -38,12 +38,23 @@ static void _logos_method$_ungrouped$NewSettingViewController$reloadTableData(_L
     WCTableViewSectionManager *section = [_logos_static_class_lookup$WCTableViewSectionManager() sectionInfoDefaut];
 
     WCTableViewCellManager *redCell = [_logos_static_class_lookup$WCTableViewCellManager() switchCellForSel:@selector(dealWithRedSwitch) target:[LFRedManager sharedInstance] title:@"红包助手" on:[LFRedManager sharedInstance].isAutoRed];
-
     [section addCell:redCell];
+
+    
+    
+
     [tbViewManager insertSection:section At:0];
     MMTableView *tableView = [tbViewManager getTableView];
     [tableView reloadData];
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -57,21 +68,51 @@ static void _logos_method$_ungrouped$WCRedEnvelopesLogicMgr$OnWCToHongbaoCommonR
 
 	NSError *err;
     NSDictionary *bufferDic = [NSJSONSerialization JSONObjectWithData:arg1.retText.buffer options:NSJSONReadingMutableContainers error:&err];
-
     
-	if ([bufferDic[@"receiveStatus"] integerValue] == 2) { return; }
-	
-	if ([bufferDic[@"hbStatus"] integerValue] == 4) { return; }
-	
-	if (!bufferDic[@"timingIdentifier"]) { return; }
-	LFRedParamManager *mgrParams = [[LFRedManager sharedInstance] getParams];
-	mgrParams.timingIdentifier = bufferDic[@"timingIdentifier"];
+    LFRedParamManager *mgrParams = [[LFRedManager sharedInstance] getParams];
+    BOOL (^isReceiveRed)()  = ^BOOL() {
+    	
+    	if (!mgrParams){return NO;}
+    	
+		if ([bufferDic[@"receiveStatus"] integerValue] == 2) { return NO; }
+		
+		if ([bufferDic[@"hbStatus"] integerValue] == 4) { return  NO; }
+		
+		if (!bufferDic[@"timingIdentifier"]) { return NO; }
 
-	
-    WCRedEnvelopesLogicMgr *redLogicMgr = [[_logos_static_class_lookup$MMServiceCenter() defaultCenter] getService:[_logos_static_class_lookup$WCRedEnvelopesLogicMgr() class]];
-    [redLogicMgr OpenRedEnvelopesRequest:[mgrParams singProtoParam]];
+		
+		BOOL (^isDescLimit)() = ^BOOL() {
+	        NSArray *descArray = @[@"错抢",@"误抢",@"双倍",@"两倍",@"三倍"];
+	        for (int i = 0; i < descArray.count; ++i)
+	        {
+	            if ([bufferDic[@"wishing"] rangeOfString:descArray[i]].location != NSNotFound) {
+	                return YES;
+	            }
+	        }
+	        return NO;
+	    };
+	    if (isDescLimit()){return NO;}
+	    
+	    return YES;
+    };
 
+    NSLog(@"打开红包参数----%@",mgrParams);
+
+    if (isReceiveRed())
+    {
+    	mgrParams.timingIdentifier = bufferDic[@"timingIdentifier"];
+
+		
+	    WCRedEnvelopesLogicMgr *redLogicMgr = [[_logos_static_class_lookup$MMServiceCenter() defaultCenter] getService:[_logos_static_class_lookup$WCRedEnvelopesLogicMgr() class]];
+	    
+	    [redLogicMgr OpenRedEnvelopesRequest:[mgrParams singProtoParam]];
+    }
 }
+
+
+
+
+
 
 
 
@@ -80,7 +121,7 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
     HBLogDebug(@"-[<CMessageMgr: %p> AsyncOnAddMsg:%@ MsgWrap:%@]", self, msg, wrap);
     _logos_orig$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(self, _cmd, msg, wrap);
 
-    NSLog(msg,wrap);
+    
     if(wrap.m_uiMessageType == 49){
 
     	
@@ -89,8 +130,31 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
         	CContactMgr *contactManager = [[_logos_static_class_lookup$MMServiceCenter() defaultCenter] getService:[_logos_static_class_lookup$CContactMgr() class]];
             CContact *selfContact = [contactManager getSelfContact];
 
+
         	
-        	if (![LFRedManager sharedInstance].isAutoRed){return;}
+        	BOOL (^isAutoRed)() = ^BOOL() {
+				return [LFRedManager sharedInstance].isAutoRed;
+			};
+        	if (!isAutoRed()){return;}
+
+        	
+        	BOOL (^isSender)() = ^BOOL() {
+				return [wrap.m_nsFromUsr isEqualToString:selfContact.m_nsUsrName];
+			};
+
+        	
+        	BOOL (^isGroup)() = ^BOOL() {
+				return [msg rangeOfString:@"@chatroom"].location != NSNotFound;
+			};
+
+			
+			if (!isGroup()){return;}
+
+			
+			BOOL (^isGroupSender)() = ^BOOL() {
+				return isSender() && [wrap.m_nsToUsr rangeOfString:@"chatroom"].location != NSNotFound;
+			};
+
 
             NSString *nativeUrl = [[wrap m_oWCPayInfoItem] m_c2cNativeUrl];
             NSString *nativeString = [nativeUrl substringFromIndex:[@"wxpay://c2cbizmessagehandler/hongbao/receivehongbao?" length]];
@@ -99,7 +163,7 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
 
 
 			NSMutableDictionary *params = [NSMutableDictionary dictionary];
-	        if ([wrap.m_nsFromUsr hasSuffix:@"@chatroom"]){ 
+	        if (isGroup()){ 
 	            [params setObject:@"0" forKey:@"inWay"]; 
 	        }else {     
 	            [params setObject:@"1" forKey:@"inWay"]; 
@@ -109,7 +173,9 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
 	        [params setObject:nativeUrlDict[@"msgtype"] forKey:@"msgType"];
 	        [params setObject:nativeUrlDict[@"channelid"] forKey:@"channelId"];
 	        [params setObject:@"0" forKey:@"agreeDuty"];
+	        NSLog(@"红包验证参数-----%@",params);
 	        WCRedEnvelopesLogicMgr *logicMgr = [[_logos_static_class_lookup$MMServiceCenter() defaultCenter] getService:[_logos_static_class_lookup$WCRedEnvelopesLogicMgr() class]];
+	        
 			[logicMgr ReceiverQueryRedEnvelopesRequest:params];
 
 			
@@ -120,7 +186,7 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
 		    mgrParams.nickName = [selfContact getContactDisplayName];
 		    mgrParams.headImg = [selfContact m_nsHeadImgUrl];
 		    mgrParams.nativeUrl = [[wrap m_oWCPayInfoItem] m_c2cNativeUrl];
-		    mgrParams.sessionUserName = wrap.m_nsFromUsr;
+		    mgrParams.sessionUserName = isGroupSender() ? wrap.m_nsToUsr : wrap.m_nsFromUsr;
 		    mgrParams.sign = nativeUrlDict[@"sign"];
             [[LFRedManager sharedInstance]addParams:mgrParams];
 
@@ -133,4 +199,4 @@ static void _logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$(_LOGOS_S
 
 static __attribute__((constructor)) void _logosLocalInit() {
 {Class _logos_class$_ungrouped$NewSettingViewController = objc_getClass("NewSettingViewController"); MSHookMessageEx(_logos_class$_ungrouped$NewSettingViewController, @selector(reloadTableData), (IMP)&_logos_method$_ungrouped$NewSettingViewController$reloadTableData, (IMP*)&_logos_orig$_ungrouped$NewSettingViewController$reloadTableData);Class _logos_class$_ungrouped$WCRedEnvelopesLogicMgr = objc_getClass("WCRedEnvelopesLogicMgr"); MSHookMessageEx(_logos_class$_ungrouped$WCRedEnvelopesLogicMgr, @selector(OnWCToHongbaoCommonResponse:Request:), (IMP)&_logos_method$_ungrouped$WCRedEnvelopesLogicMgr$OnWCToHongbaoCommonResponse$Request$, (IMP*)&_logos_orig$_ungrouped$WCRedEnvelopesLogicMgr$OnWCToHongbaoCommonResponse$Request$);Class _logos_class$_ungrouped$CMessageMgr = objc_getClass("CMessageMgr"); MSHookMessageEx(_logos_class$_ungrouped$CMessageMgr, @selector(AsyncOnAddMsg:MsgWrap:), (IMP)&_logos_method$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$, (IMP*)&_logos_orig$_ungrouped$CMessageMgr$AsyncOnAddMsg$MsgWrap$);} }
-#line 108 "Red/Tweak.xm"
+#line 174 "Red/Tweak.xm"
--- Red/Tweak.xm
+++ Red/Tweak.xm
@@ -12,12 +12,23 @@
     WCTableViewSectionManager *section = [%c(WCTableViewSectionManager) sectionInfoDefaut];
 
     WCTableViewCellManager *redCell = [%c(WCTableViewCellManager) switchCellForSel:@selector(dealWithRedSwitch) target:[LFRedManager sharedInstance] title:@"红包助手" on:[LFRedManager sharedInstance].isAutoRed];
-
     [section addCell:redCell];
+
+    //WCTableViewCellManager *setCell = [%c(WCTableViewCellManager) normalCellForSel:@selector(settingRedView) target:self title:@"微信助手"];
+    // [section addCell:setCell];
+
     [tbViewManager insertSection:section At:0];
     MMTableView *tableView = [tbViewManager getTableView];
     [tableView reloadData];
 }
+
+// %new 
+// -(void)settingRedView
+// {
+// 	MySettingViewController *myView = [[MySettingViewController alloc]init];
+//     [self.navigationController pushViewController:myView animated:YES];
+// }
+
 %end
 
 
@@ -31,30 +42,60 @@
 
 	NSError *err;
     NSDictionary *bufferDic = [NSJSONSerialization JSONObjectWithData:arg1.retText.buffer options:NSJSONReadingMutableContainers error:&err];
+    // 打开红包的参数
+    LFRedParamManager *mgrParams = [[LFRedManager sharedInstance] getParams];
+    BOOL (^isReceiveRed)()  = ^BOOL() {
+    	// 非自动抢红包
+    	if (!mgrParams){return NO;}
+    	// 已抢过
+		if ([bufferDic[@"receiveStatus"] integerValue] == 2) { return NO; }
+		// 红包被抢完
+		if ([bufferDic[@"hbStatus"] integerValue] == 4) { return  NO; }
+		//判断是否是外挂
+		if (!bufferDic[@"timingIdentifier"]) { return NO; }
 
-    // 已抢过
-	if ([bufferDic[@"receiveStatus"] integerValue] == 2) { return; }
-	// 红包被抢完
-	if ([bufferDic[@"hbStatus"] integerValue] == 4) { return; }
-	//判断是否是外挂
-	if (!bufferDic[@"timingIdentifier"]) { return; }
-	LFRedParamManager *mgrParams = [[LFRedManager sharedInstance] getParams];
-	mgrParams.timingIdentifier = bufferDic[@"timingIdentifier"];
+		// 判断红包上的描述
+		BOOL (^isDescLimit)() = ^BOOL() {
+	        NSArray *descArray = @[@"错抢",@"误抢",@"双倍",@"两倍",@"三倍"];
+	        for (int i = 0; i < descArray.count; ++i)
+	        {
+	            if ([bufferDic[@"wishing"] rangeOfString:descArray[i]].location != NSNotFound) {
+	                return YES;
+	            }
+	        }
+	        return NO;
+	    };
+	    if (isDescLimit()){return NO;}
+	    
+	    return YES;
+    };
 
-	//WCRedEnvelopesLogicMgr
-    WCRedEnvelopesLogicMgr *redLogicMgr = [[%c(MMServiceCenter) defaultCenter] getService:[%c(WCRedEnvelopesLogicMgr) class]];
-    [redLogicMgr OpenRedEnvelopesRequest:[mgrParams singProtoParam]];
+    NSLog(@"打开红包参数----%@",mgrParams);
 
+    if (isReceiveRed())
+    {
+    	mgrParams.timingIdentifier = bufferDic[@"timingIdentifier"];
+
+		//WCRedEnvelopesLogicMgr
+	    WCRedEnvelopesLogicMgr *redLogicMgr = [[%c(MMServiceCenter) defaultCenter] getService:[%c(WCRedEnvelopesLogicMgr) class]];
+	    // 打开红包
+	    [redLogicMgr OpenRedEnvelopesRequest:[mgrParams singProtoParam]];
+    }
 }
 
 %end
 
 %hook CMessageMgr
+/**
+msg当前聊天消息的对象，群格式17733384035@chatroom，个人wxid_s8fx99ltaybp12
+自己在群里发消息，判断m_nsToUsr是否包含chatroom
+他人在群里发消息，判断m_nsFromUsr是否包含chatroom
+*/
 - (void)AsyncOnAddMsg:(NSString *)msg MsgWrap:(CMessageWrap *)wrap{
     %log;
     %orig;
 
-    NSLog(msg,wrap);
+    // NSLog(msg,wrap);
     if(wrap.m_uiMessageType == 49){
 
     	//红包
@@ -63,8 +104,31 @@
         	CContactMgr *contactManager = [[%c(MMServiceCenter) defaultCenter] getService:[%c(CContactMgr) class]];
             CContact *selfContact = [contactManager getSelfContact];
 
+
         	// 自动抢红包开关
-        	if (![LFRedManager sharedInstance].isAutoRed){return;}
+        	BOOL (^isAutoRed)() = ^BOOL() {
+				return [LFRedManager sharedInstance].isAutoRed;
+			};
+        	if (!isAutoRed()){return;}
+
+        	//是否是自己发消息（红包）
+        	BOOL (^isSender)() = ^BOOL() {
+				return [wrap.m_nsFromUsr isEqualToString:selfContact.m_nsUsrName];
+			};
+
+        	//是否是群消息（红包）
+        	BOOL (^isGroup)() = ^BOOL() {
+				return [msg rangeOfString:@"@chatroom"].location != NSNotFound;
+			};
+
+			//个人红包不自动领
+			if (!isGroup()){return;}
+
+			// 是否自己在群聊中发消息（红包）
+			BOOL (^isGroupSender)() = ^BOOL() {
+				return isSender() && [wrap.m_nsToUsr rangeOfString:@"chatroom"].location != NSNotFound;
+			};
+
 
             NSString *nativeUrl = [[wrap m_oWCPayInfoItem] m_c2cNativeUrl];
             NSString *nativeString = [nativeUrl substringFromIndex:[@"wxpay://c2cbizmessagehandler/hongbao/receivehongbao?" length]];
@@ -73,7 +137,7 @@
 
 
 			NSMutableDictionary *params = [NSMutableDictionary dictionary];
-	        if ([wrap.m_nsFromUsr hasSuffix:@"@chatroom"]){ //群红包
+	        if (isGroup()){ //群红包
 	            [params setObject:@"0" forKey:@"inWay"]; //0:群聊，1：单聊
 	        }else {     //个人红包
 	            [params setObject:@"1" forKey:@"inWay"]; 
@@ -83,7 +147,9 @@
 	        [params setObject:nativeUrlDict[@"msgtype"] forKey:@"msgType"];
 	        [params setObject:nativeUrlDict[@"channelid"] forKey:@"channelId"];
 	        [params setObject:@"0" forKey:@"agreeDuty"];
+	        NSLog(@"红包验证参数-----%@",params);
 	        WCRedEnvelopesLogicMgr *logicMgr = [[%c(MMServiceCenter) defaultCenter] getService:[%c(WCRedEnvelopesLogicMgr) class]];
+	        //发送红包验证消
 			[logicMgr ReceiverQueryRedEnvelopesRequest:params];
 
 			// 存储打开红包的参数
@@ -94,7 +160,7 @@
 		    mgrParams.nickName = [selfContact getContactDisplayName];
 		    mgrParams.headImg = [selfContact m_nsHeadImgUrl];
 		    mgrParams.nativeUrl = [[wrap m_oWCPayInfoItem] m_c2cNativeUrl];
-		    mgrParams.sessionUserName = wrap.m_nsFromUsr;
+		    mgrParams.sessionUserName = isGroupSender() ? wrap.m_nsToUsr : wrap.m_nsFromUsr;
 		    mgrParams.sign = nativeUrlDict[@"sign"];
             [[LFRedManager sharedInstance]addParams:mgrParams];
 
